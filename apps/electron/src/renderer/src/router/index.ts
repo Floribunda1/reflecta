e9abff26fac787314c1fec0a56533a5c1f0abd35
Floribunda1 <1094083688@qq.com>
2026-04-29 10:43:12 +0800

    refactor: remove wireframe

--- apps/electron/src/renderer/src/router/index.ts
+++ apps/electron/src/renderer/src/router/index.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import { AppLayout } from "@renderer/modules/shared/layout/AppLayout";
 import { CapturePage } from "@renderer/modules/capture";
 import { ContemplatePage } from "@renderer/modules/contemplate";
-import { V2WireframePage } from "@renderer/modules/v2-wireframe";
 
 export const router = createRouter({
   history: createWebHashHistory(),
@@ -14,7 +13,6 @@ export const router = createRouter({
       children: [
         { path: "/capture", name: "Capture", component: CapturePage },
         { path: "/contemplate", name: "Contemplate", component: ContemplatePage },
-        { path: "/v2-wireframe", name: "V2 Wireframe", component: V2WireframePage },
       ],
     },
   ],
