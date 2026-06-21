37069ef3abdcc1d171f1eb71ec98b35d9b461043
Floribunda1 <1094083688@qq.com>
2026-06-22 00:11:35 +0800

    fix(electron): allow multiple gui instances

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -11,7 +11,6 @@ import { preloadScript, rendererHtml } from "./paths";
 registerAssetScheme();
 app.setName(APP_NAME);
 initializeLogging();
-const gotSingleInstanceLock = app.requestSingleInstanceLock();
 
 const createWindow = (option?: Electron.BrowserWindowConstructorOptions, route?: string) => {
   // Create the browser window.
@@ -75,49 +74,38 @@ const createWindow = (option?: Electron.BrowserWindowConstructorOptions, route?:
   }
 };
 
-if (!gotSingleInstanceLock) {
-  app.quit();
-} else {
-  app.on("second-instance", () => {
-    const [window] = BrowserWindow.getAllWindows();
-    if (!window) return;
-    if (window.isMinimized()) window.restore();
-    window.focus();
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(async () => {
+  await initializeDB();
+
+  nativeTheme.themeSource = "system";
+
+  // Serve local assets via asset:// protocol
+  handleAssetProtocol();
+
+  // Set app user model id for windows
+  electronApp.setAppUserModelId("com.electron");
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
   });
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.whenReady().then(async () => {
-    await initializeDB();
+  // IPC test
+  ipcMain.on("ping", () => appLog.debug("ipc.ping"));
 
-    nativeTheme.themeSource = "system";
+  createWindow();
 
-    // Serve local assets via asset:// protocol
-    handleAssetProtocol();
-
-    // Set app user model id for windows
-    electronApp.setAppUserModelId("com.electron");
-
-    // Default open or close DevTools by F12 in development
-    // and ignore CommandOrControl + R in production.
-    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-    app.on("browser-window-created", (_, window) => {
-      optimizer.watchWindowShortcuts(window);
-    });
-
-    // IPC test
-    ipcMain.on("ping", () => appLog.debug("ipc.ping"));
-
-    createWindow();
-
-    app.on("activate", () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
-}
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
